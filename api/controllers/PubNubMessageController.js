13a64654159ca69950ac2bde89edf61fdfef50ce
enicky <enicky80@gmail.com>
2016-02-01 08:18:39 +0100

    Angle Additions

---
+++ api/controllers/PubNubMessageController.js
@@ -0,0 +1,11 @@
+/**
+ * PubNubMessageController
+ *
+ * @description :: Server-side logic for managing Pubnubmessages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
